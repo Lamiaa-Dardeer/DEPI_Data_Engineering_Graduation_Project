076a91090f6c033890bf981066732fdb950ff4fc
Lamiaa-Dardeer <Lamiaa.hatem50@gmail.com>
2026-03-12 23:50:33 +0200

    Create materialized view for pollution_raw_data
    Add a new materialized view for pollution_raw_data.

--- models/refresh_model.sql
+++ models/refresh_model.sql
@@ -1,1 +1,7 @@
+{{ config(materialized='table') }}
 
+-- هذا السطر يقرأ البيانات من جدولك الحالي ويصنع منه نسخة جديدة
+-- النسخة الجديدة ستحصل على 60 يوماً إضافية تلقائياً
+SELECT 
+    * FROM 
+    `depi-graduation-project-489604.staging_area.pollution_raw_data`
