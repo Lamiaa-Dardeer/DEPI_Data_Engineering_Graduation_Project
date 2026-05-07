e63f7e11e7d3114e77b7aa8f45a31164f2e76c7f
Lamiaa Dardeer <lamiaa.hatem50@gmail.com>
2026-05-07 09:03:43 +0300

    someting in main? it was just a save all & commit message that I agree on

--- logs/query_log.sql
+++
@@ -1,9 +0,0 @@
--- created_at: 2026-05-06T13:32:07.446737400+00:00
--- finished_at: 2026-05-06T13:32:12.215177300+00:00
--- elapsed: 4.8s
--- outcome: success
--- dialect: bigquery
--- node_id: not available
--- query_id: yCwoOrAEDV3GHyG4mv1ZjJc9TrL
--- desc: not provided
-select 1 as id;
