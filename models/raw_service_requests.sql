179b39bdb65fddcfd8a345b1abe42d23dd9a8786
Lamiaa Dardeer <lamiaa.hatem50@gmail.com>
2026-05-22 15:26:42 +0300

    Update README and archive structure

--- models/raw_service_requests.sql
+++ models/raw_service_requests.sql
@@ -1,5 +1,7 @@
-{{ config(materialized='table') }}
+{{ config(
+    materialized='table',
+    alias='raw_service_requests'
+) }}
 
--- هذا الكود يقرأ من الجدول الخام ويخلق جدولاً جديداً تماماً لتصفير العداد
-SELECT * 
-FROM `depi-graduation-project-489604.nyc_311_raw_data.raw_service_requests`
+-- هذا الكود يقرأ من الجدول المخزن حالياً، ويعيد بناءه وتصفير عداد الـ 60 يوماً
+SELECT * FROM `depi-graduation-project-489604.nyc_311_raw_data.raw_service_requests`
